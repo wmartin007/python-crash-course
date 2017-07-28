f9b7ba6def813e98aab03102a1c064d881417817
Unknown <wmartin007@gmail.com>
2017-07-28 18:59:44 -0400

    7/28 Ch 7 Updates

--- python_work/ch7_UserInput_WhileLops.py
+++ python_work/ch7_UserInput_WhileLops.py
@@ -173,4 +173,35 @@ while current_number < 10:
 ## Moving Items from One List to Another
 #	Consider a list of newly registered but unverified users of a 
 # website. After we verify the users, how can we move them to a separate
-# list of confirmed users
+# list of confirmed users? One way would be t use a while loop to pull
+# users from the list of unconfirmed users as we verify them and then
+# add them to a separate listof confirmed users.
+
+# Start with users that need to be verified, and an empty list to hold
+#  confirmed users.
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+
+# Verify each user until there are no more unconfirmed users.
+#  Move each verified user into the list of confirmed users.
+while unconfirmed_users:
+	current_user = unconfirmed_users.pop()
+	
+	print("Verifying user: " + current_user.title())
+	confirmed_users.append(current_user)
+# Display all confirmed users.
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+	print(confirmed_user.title())
+
+## Removing All Instances of Specific Values from a List
+#	In Chapter 3 we used remove() to remove a specific value from a list.
+# This only worked on the first instance of a value from the list.
+
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+while 'cat' in pets:
+	pets.remove('cat')
+
+print(pets)
