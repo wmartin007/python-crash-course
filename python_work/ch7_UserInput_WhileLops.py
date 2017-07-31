1430fb08f3eb1667444e7cadba1aed61e7287c68
Unknown <wmartin007@gmail.com>
2017-07-31 06:54:36 -0400

    7/31 Ch 7 Updates

--- python_work/ch7_UserInput_WhileLops.py
+++ python_work/ch7_UserInput_WhileLops.py
@@ -201,7 +201,39 @@ for confirmed_user in confirmed_users:
 pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
 print(pets)
 
+
 while 'cat' in pets:
 	pets.remove('cat')
 
 print(pets)
+
+
+## Filling a Dictionary with User Input
+#	You can prompt for as much input as you as you need in each pass 
+# through a while loop. Let's make a polling program in which each pass
+# through the loop prompts for the participant's name and response. 
+
+responses = {}
+
+# Set a flag to indicate that polling is active.
+polling_active = True
+
+while polling_active:
+	#Prompt for person's name and response.
+	name = input("\nWhat is your name? ")
+	reponse = input("Which mountain would you like to climb someday? ")
+	
+	#Store the response in the dictionary:
+	responses[name]=response
+	
+	#Find out if anyone else is going to take the poll.
+	repeat = input("Would you like to let another person respond? " +
+		("(yes/no) ")
+	if repeat == 'no':
+		polling_active = False
+	
+# Polling is complete. Show the results.
+("\n---Poll Results---")
+for name, response in response.items():
+	print(name + " would like to climb " + response + ".")
+	
