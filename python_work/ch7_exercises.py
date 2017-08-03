cafc9d533202040df318438f48458b976325da64
Unknown <wmartin007@gmail.com>
2017-08-03 17:33:35 -0400

    8/3 Ch 7 Updates

--- python_work/ch7_exercises.py
+++ python_work/ch7_exercises.py
@@ -63,3 +63,17 @@ while True:
 	else:
 		print("Price: $15")
 	break
+
+
+## 7.6 Deli
+#Make a list called sandwich_orders and fill it with the names of various
+#sandwiches. Then make an empty list called finished_sandwiches. Loop
+#through the list of sandwich orders and print a message for each order, such
+#as I made your tuna sandwich. As each sandwich is made, move it to the list
+#of finished sandwiches. After all the sandwiches have been made, print a
+#message listing each sandwich that was made.
+
+sandwich_orders = ['turkey', 'italian',  'blt', 'grilled cheese']
+
+
+
