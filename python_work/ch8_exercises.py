cb650800ea0865568d0690ae09964270bd6d491c
Unknown <wmartin007@gmail.com>
2017-08-16 17:35:59 -0400

    8/16 Ch 8 Updates

--- python_work/ch8_exercises.py
+++ python_work/ch8_exercises.py
@@ -60,7 +60,65 @@ describe_city('las vegas')
 describe_city('paris','france')
 
 
+## 8.6 City Names
+# Write a function called city_country() that takes in the name
+# of a city and its country. The function should return a string formatted like
+# this:
+# "Santiago, Chile"
+# Call your function with at least three city-country pairs, and print the value
+# that’s returned.
 def city_country(city, country):
     return(city.title() + ", " + country.title())
 
 print(city_country('santiago', 'chile'))
+print(city_country('berlin', 'germany'))
+print(city_country('paris', 'france'))
+
+## 8.7 Album
+# Write a function called make_album() that builds a dictionary
+# describing a music album. The function should take in an artist name and an
+# album title, and it should return a dictionary containing these two pieces of
+# information. Use the function to make three dictionaries representing different
+# albums. Print each return value to show that the dictionaries are storing the
+# album information correctly.
+# Add an optional parameter to make_album() that allows you to store the
+# number of tracks on an album. If the calling line includes a value for the number
+# of tracks, add that value to the album’s dictionary. Make at least one new
+# function call that includes the number of tracks on an album.
+def make_album(artist, title, num_tracks = ''):
+    album = {'artist': artist, 'title': title}
+    if num_tracks:
+        album['# of tracks'] = num_tracks
+    return album
+
+garth = make_album('garth brooks',"garth's greatest hits")
+print(garth)
+
+kenny = make_album('kenny chesney', 'road and the radio', 12)
+print(kenny)
+
+## 8.8 User Albums
+# Start with your program from Exercise 8-7. Write a while
+# loop that allows users to enter an album’s artist and title. Once you have
+# that information, call make_album() with the user’s input and print the
+# dictionary that’s created. Be sure to include a quit value in the while loop.
+
+while True:
+    print("\nEnter the album information:")
+    print("(enter 'q' at any time to quit)")
+
+    artist = input("Artist's name: ")
+    if artist == 'q':
+        break
+
+    album = input("Album title: ")
+    if album == 'q':
+        break
+
+    tracks = input("Number of tracks on album (leave blank if unknown): ")
+    if tracks == 'q':
+        break
+
+    print(make_album(artist, album, tracks))
+
+
